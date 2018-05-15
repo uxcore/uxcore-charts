952d20b9d7442421058e6c7f52ca4396a68b73d0
光弘 <guanghong.wsj@alibaba-inc.com>
2018-05-15 10:11:46 +0800

    init

---
+++ demo/index.jsx
@@ -0,0 +1,14 @@
+/**
+ * Charts Component Demo for uxcore
+ * @author eternalsky
+ *
+ * Copyright 2017-2018, Uxcore Team, Alinw.
+ * All rights reserved.
+ */
+
+import ReactDOM from 'react-dom';
+import React from 'react';
+import Demo from './ChartsDemo';
+
+ReactDOM.render(<Demo />, document.getElementById('UXCoreDemo'));
+
---
+++ src/Charts.jsx
@@ -0,0 +1,33 @@
+/**
+ * Charts Component for uxcore
+ * @author eternalsky
+ *
+ * Copyright 2017-2018, Uxcore Team, Alinw.
+ * All rights reserved.
+ */
+import React from 'react';
+
+class Charts extends React.Component {
+
+  static defaultProps = {
+  };
+
+  static propTypes = {
+  };
+
+  static displayName = 'Charts';
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>uxcore-charts component</div>
+    );
+  }
+}
+
+export default Charts;
+
